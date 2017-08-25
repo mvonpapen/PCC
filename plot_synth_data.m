6f33d5efe1944fa799030826898e5ed66289be28
mvonpapen <m.von.papen@fz-juelich.de>
2017-08-25 16:22:53 +0200

    added wavelet reconstruction

--- plot_synth_data.m
+++ plot_synth_data.m
@@ -2,22 +2,17 @@
 
 
 %%  Choose a method (PCC, ImCoh, wPLI)
-%% PCC
-meth = 'PCC';
-synth_data_PCC
+% % PCC
+% meth = 'PCC';
+% synth_data_PCC %generates synthetic data and applied PCC
 
-%% IC
-% meth = 'IC';
-% synth_data_IC
-
-%% PLI
-% meth = 'wPLI';
-% synth_data_PLI
-
-%%
+% PLI
+meth = 'wPLI';
+synth_data_PLI %generates synthetic data and applied PCC based on wPLI
 
 
-% Figure
+
+%% Figure
 fig1 = figure('Papersize', [16 10], 'PaperPosition', [0.75 0.5 14.5 9], ...
         'PaperPositionmode', 'manual', 'Visible', 'on'); 
     
@@ -31,27 +26,21 @@ t     = t(1:ds:end);
 limy  = [1e-8 1e-6];
 
 
-
-%% PCC
+%% Compute mean and error
+% PCC
 if strcmp(meth,'PCC')
-    M = [mean(Ptot,2) mean(Pinc,2) mean(Pcoh,2) mean(Pvc,2)+1e-19]';
+    M = [mean(Ptot,2) mean(Pinc,2) mean(Pcoh,2) mean(Pvc,2)]';
     lineprops.col={'k'; 'r'; 'b'; 'g'};
-    E = [ min([mean(Ptot,2)'-1e-20; std(Ptot,0,2)']); ...
-        min([mean(Pinc,2)'-1e-20; std(Pinc,0,2)']);...
-        min([mean(Pcoh,2)'-1e-20; std(Pcoh,0,2)']);...
-        min([mean(Pvc, 2)'-1e-20; std(Pvc, 0,2)']) ]; %% CHECKEN!
+    E = [ std(Ptot,0,2) std(Pinc,0,2) std(Pcoh,0,2) std(Pvc, 0,2) ]';
 end
-
-%% IC or PLI
+% IC or PLI
 if any(strcmp(meth,{'wPLI', 'PLI', 'IC'}))
     M = [mean(Ptot,2) mean(Pinc,2) mean(Pcoh,2)]';
     lineprops.col={'k'; 'r'; 'b'};
-    E = [ std(Ptot,0,2) std(Pinc,0,2) std(Pcoh,0,2) ]'; %% CHECKEN!
-    E = min(cat(3, E, M-(1e-10)),[],3);
+    E = [ std(Ptot,0,2) std(Pinc,0,2) std(Pcoh,0,2) ]';
 end
 
-%%
-
+%% Plot results
 mseb(f, M, E, lineprops);
 xlim([1 60])
 hold all
@@ -75,27 +64,68 @@ clear Psig
 [~,~,~,Psig(:,4)] = preprocdata(xg , 'freq', f, 'w0', w0, 'dt', dt);
 loglog(f,Psig,'k--');
 
-%% PCC
-% h = legend('total (sine+noise)', 'incoherent', 'coherent', ...
-%     'vol.cond.', 'sine w/o noise');
+%% Set Legend, Axes, and Title
+if strcmp(meth,'PCC')
+    h = legend('total (sine+noise)', 'incoherent', 'coherent', ...
+        'vol.cond.', 'sine w/o noise');
+end
+% IC or PLI
+if any(strcmp(meth,{'wPLI', 'PLI', 'IC'}))
+    h = legend('total (sine+noise)', 'incoherent', 'coherent', ...
+        'sine w/o noise');
+end
 
-%% IC or PLI
-h = legend('total (sine+noise)', 'incoherent', 'coherent', ...
-    'sine w/o noise');
-
-%%
-
-% Set Legend, Axes, and Title
 set(h, 'Interpreter', 'Latex');
 ylim([1e-8 1e-6])
 set(gca, 'Ysca', 'log')
 xlim([1 70])
 xlabel('f [Hz]', 'Interpreter', 'Latex')
 ylabel('PSD [V$^2$/Hz]', 'Interpreter', 'Latex')
-title([meth ', \omega_0=' num2str(w0) ', n_\sigma=' num2str(nsig) '$'], ...
+title([meth ', $\omega_0=' num2str(w0) ', n_\sigma=' num2str(nsig) '$'], ...
     'Interpreter', 'Latex');
 
 % Save and close
 % print(fig1, fname, '-depsc')
 % close(fig1)
 set(fig1, 'Visi', 'On')
+
+
+%% Reconstruct time series for PCC only at the moment
+if strcmp(meth,'PCC')
+    % Figure
+    fig1 = figure('Papersize', [10 4], 'PaperPosition', [0.75 0.5 8.5 3], ...
+            'PaperPositionmode', 'manual', 'Visible', 'on');
+    
+    % Define W_coh, W_inc and W_vc
+    Ph  = angle(Wxy(:,:,1,2))/pi*180;
+    coh = C(:,:,1,2)>sig & abs(Ph)>phase_thresh;
+    inc = C(:,:,1,2)<=sig;
+    vc  = C(:,:,1,2)>sig & abs(Ph)<=phase_thresh;
+
+    % Define the modified matrices of wavelet coefficients as given in Equations (8)-(10)
+    Wcoh = W1(:,1:ds:end,1);
+    Wcoh(~coh) = 0;
+    Winc = W1(:,1:ds:end,1);
+    Winc(~inc) = 0;
+    Wvc  = W1(:,1:ds:end,1);
+    Wvc(~vc) = 0;
+
+    % wavelet reconstruction
+    [ts_coh, P_coh] = wave_recstr ( Wcoh, f, w0 );
+    [ts_inc, P_inc] = wave_recstr ( Winc, f, w0 );
+    [ts_vc,  P_vc]  = wave_recstr ( Wvc, f, w0 );
+    [ts_tot, P_tot, varWT] = wave_recstr ( W1(:,1:ds:end,1), f, w0 );
+    fprintf('Variance of original and reconstructed time series : %f/%f.\n', ...
+        sigma2, varWT)
+    fprintf('Error of %f%%.\n', (sigma2-varWT)/sigma2)
+
+    plot(t, ts_vc+0.015, 'g-', t, ts_inc-0.015, 'r-', t, ts_coh, 'b-')
+    xlim([0 T])
+    ylim([-0.025 0.025]);
+    xlabel('time [s]', 'interp', 'latex')
+    ylabel('PCC signals [V]', 'interp', 'latex') 
+
+    %% Plot original and reconstructed time series
+    %% this only looks good if frequency resolution is sufficiently high
+    % figure, plot(t,X(:,1), t,ts_tot)
+end
